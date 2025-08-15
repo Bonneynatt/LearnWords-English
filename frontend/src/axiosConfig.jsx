5d296157f7ef0cce46ea5f0cb6c6e1a9745e2214
Bonneynatt <n11835133@qut.edu.au>
2025-08-16 09:19:25 +1000

    change IP again

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // Updated to run locally
-  // baseURL: 'http://3.107.50.175:5001', // Updated to live
+  // baseURL: 'http://13.210.14.164:5001', // Updated to live
   headers: { 'Content-Type': 'application/json' },
 });
 
