04a48d8da8a485c6d21c17c2f15bcac17e4041c2
Bonneynatt <n11835133@qut.edu.au>
2025-08-16 21:19:32 +1000

    LWE-33 add flashcard browser

--- frontend/src/components/flashcards/FlashcardBrowser.jsx
+++ frontend/src/components/flashcards/FlashcardBrowser.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import flashcardService from '../../services/flashcardService';
-//This is FlashcardBrowser.jsx.
+//This is FlashcardBrowser.jsxgit
 const FlashcardBrowser = () => {
   const navigate = useNavigate();
   const [flashcards, setFlashcards] = useState([]);
