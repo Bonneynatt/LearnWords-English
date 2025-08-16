bfda65a6a6b3c68be270d8f1b6c3b02a202e1860
Bonneynatt <n11835133@qut.edu.au>
2025-08-16 14:20:43 +1000

    updated axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -6,31 +6,5 @@ const axiosInstance = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 
-// Add request interceptor to include token
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Add response interceptor to handle errors
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default axiosInstance;
