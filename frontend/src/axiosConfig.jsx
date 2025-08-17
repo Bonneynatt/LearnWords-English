4a43c74b9c7d1c415bcb79e4b1d1e874602a6266
Bonneynatt <n11835133@qut.edu.au>
2025-08-17 18:23:11 +1000

    change my ip

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,35 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api', // Updated to include /api
+  baseURL: 'http://localhost:5001', // Updated to include /api
+  //baseURL: 'http://13.211.99.24:5001', //live
   headers: { 'Content-Type': 'application/json' },
 });
 
-// Add request interceptor to include token
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
-// Add response interceptor to handle errors
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default axiosInstance;
